919d736e0fb42b3e2158901a53d6c06fc8f84995
carlkiptoo <carlosiamcarl@gmail.com>
2025-02-19 16:23:52 +0300

    setting up Mongo complete

---
+++ backend/config/db.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import Drink from '../models/Drink.js';
+
+const router = express.Router();
+
+//GEt all drinks
+router.get('/', async (req, res) => {
+    const drinks = await Drink.find();
+    res.json(drinks);
+});
+
+//Add a new drink
+router.post('/', async (req, res) => {
+    const newDrink = new Drink(req.body)
+    await newDrink.save()
+    res.status(201).json(newDrink)
+});
+
+//Update Drink
+router.put('/:id', async (req, res) => {
+    await Drink.findByIdAndUpdate(req.params.id, req.body);
+    res.sendStatus(200);
+})
+
+//Delete drink
+router.delete('/:id', async (req, res) => {
+    await Drink.findByIdAndDelete(req.params.id);
+    res.sendStatus(200);
+});
+
+export default router;
